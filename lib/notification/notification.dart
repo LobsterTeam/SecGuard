8c00db619688159d48875eb10adee179b40d32a0
gozeloglu <gozeloglu@gmail.com>
2020-11-28 18:20:59 +0300

    customCard is created

--- lib/notification/notification.dart
+++ lib/notification/notification.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 
 class NotificationPage extends StatefulWidget {
+  NotificationPage(
+      {Key key,
+      this.androidVersion,
+      this.vendorPatchLevel,
+      this.androidPathLevel})
+      : super(key: key);
+
+  final String androidVersion;
+  final String vendorPatchLevel;
+  final String androidPathLevel;
+
   @override
   State<StatefulWidget> createState() {
     return _NotificationState();
@@ -10,6 +21,32 @@ class NotificationPage extends StatefulWidget {
 class _NotificationState extends State<NotificationPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          // TODO the second parameter will be replaced with widget.androidVersion
+          customCard("Android Version", "9"),
+          // TODO the second parameter will be replaced with widget.vendorPatchLevel
+          customCard("Vendor security patch level", "01 September 2020"),
+          // TODO the second parameter will be replaced with widget.androidPatchLevel
+          customCard("Android security patch level", "01 October 2020"),
+        ],
+      ),
+    );
+  }
+
+  Card customCard(String title, String subtitle) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          title,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+        ),
+        subtitle: Text(
+          subtitle,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
+        ),
+      ),
+    );
   }
 }
