8c49cbb8b861e0d01d698fb6c726abf2b635d2ed
fidansamet <fidansamet@hacettepe.edu.tr>
2020-11-29 00:51:57 +0300

    home cards added

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -4,6 +4,7 @@ class ThemeColors {
   const ThemeColors();
 
   static const Color redViolet = const Color(0xff922b3e);
+  static const Color lightRedViolet = const Color(0xaf922b3e);
   static const Color redCard = const Color(0xffff0000);
   static const Color orangeCard = const Color(0xffffa500);
   static const Color yellowOrangeCard = const Color(0xffffae42);
@@ -20,5 +21,24 @@ class ThemeTextStyles {
       fontFamily: 'Poppins',
       fontWeight: FontWeight.w600,
       fontSize: 30.0
+  );
+
+  static const TextStyle homeCardTitle = const TextStyle(
+      color: Colors.white,
+      fontFamily: 'Poppins',
+      fontWeight: FontWeight.w600,
+      fontSize: 20.0
+  );
+
+  static const TextStyle rowKey = const TextStyle(
+      color: Colors.white,
+      fontWeight: FontWeight.w300,
+      fontSize: 16.0
+  );
+
+  static const TextStyle rowValue = const TextStyle(
+      color: Colors.white,
+      fontWeight: FontWeight.w600,
+      fontSize: 16.0
   );
 }
