40de72b392e540dc3d0c2a6fd50a1d9850989b19
fidansamet <fidansamet@hacettepe.edu.tr>
2020-11-29 11:48:20 +0300

    device info cards added to home tab

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,23 +1,62 @@
 import 'package:flutter/material.dart';
 import '../utils/theme.dart';
+import 'package:device_info/device_info.dart';
 
-class HomeTab extends StatelessWidget {
-  HomeTab();
+class HomeTab extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeTabState();
+  }
+}
+
+class _HomeTabState extends State<HomeTab> {
+  bool loading = true;
+  Map<String, String> deviceInfoMap = {};
+
+  @override
+  void initState() {
+    getDeviceInfo();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      child: new ListView.builder(
-        itemExtent: 160.0,
-        itemCount: 2,
-        itemBuilder: (_, index) => createCard(index),
-      ),
-    );
+    if (loading) {
+      return Center(
+          child: CircularProgressIndicator(
+              valueColor:
+                  AlwaysStoppedAnimation<Color>(ThemeColors.redViolet)));
+    } else {
+      return new Container(
+        child: new ListView.builder(
+          itemCount: 2,
+          itemBuilder: (_, index) => createCard(index),
+        ),
+      );
+    }
+  }
+
+  Future<Map<String, String>> getDeviceInfo() async {
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    AndroidDeviceInfo androidDeviceInfo = await deviceInfo.androidInfo;
+
+    deviceInfoMap["manufacturer"] = androidDeviceInfo.manufacturer;
+    deviceInfoMap["model"] = androidDeviceInfo.model;
+    deviceInfoMap["board"] = androidDeviceInfo.board;
+    deviceInfoMap["hardware"] = androidDeviceInfo.hardware;
+    deviceInfoMap["version"] = androidDeviceInfo.version.release;
+    deviceInfoMap["API"] = androidDeviceInfo.version.sdkInt.toString();
+    deviceInfoMap["secPatch"] = androidDeviceInfo.version.securityPatch;
+    deviceInfoMap["googlePlaySecPatch"] =
+        androidDeviceInfo.version.securityPatch;
+
+    setState(() {
+      loading = false;
+    });
   }
 
   Container createCard(int type) {
     return new Container(
-      height: 120.0,
+      height: type == 0 ? 160.0 : 140.0,
       margin: const EdgeInsets.only(top: 20.0, bottom: 8.0),
       child: new Stack(
         children: <Widget>[
@@ -26,8 +65,7 @@ class HomeTab extends StatelessWidget {
             decoration: new BoxDecoration(
                 color: ThemeColors.lightRedViolet,
                 shape: BoxShape.rectangle,
-                borderRadius: new BorderRadius.circular(8.0)
-            ),
+                borderRadius: new BorderRadius.circular(8.0)),
             child: new Container(
               margin: const EdgeInsets.only(top: 16.0, left: 16.0),
               constraints: new BoxConstraints.expand(),
@@ -44,97 +82,58 @@ class HomeTab extends StatelessWidget {
 
   List<Widget> hardwareWidgets() {
     return <Widget>[
-      new Text(
-          "Hardware",
-          style: ThemeTextStyles.homeCardTitle
-      ),
+      new Text("Hardware", style: ThemeTextStyles.homeCardTitle),
       new Container(
         color: Colors.white,
         width: 100.0,
         height: 1.0,
       ),
+      new Padding(padding: EdgeInsets.only(top: 10.0)),
+      createRow("Manufacturer", deviceInfoMap["manufacturer"]),
       new Padding(padding: EdgeInsets.only(top: 5.0)),
-      new Row(
-        children: <Widget>[
-          Text(
-            "key",
-            textAlign: TextAlign.left,
-            style: ThemeTextStyles.rowKey,
-          ),
-          Padding(padding: EdgeInsets.only(right: 15.0)),
-          Text(
-            "value",
-            textDirection: TextDirection.ltr,
-            textAlign: TextAlign.left,
-            style: ThemeTextStyles.rowValue,
-          )
-        ],
-      ),
+      createRow("Model", deviceInfoMap["model"]),
       new Padding(padding: EdgeInsets.only(top: 5.0)),
-      new Row(
-        children: <Widget>[
-          Text(
-            "key",
-            textAlign: TextAlign.left,
-            style: ThemeTextStyles.rowKey,
-          ),
-          Padding(padding: EdgeInsets.only(right: 15.0)),
-          Text(
-            "value",
-            textDirection: TextDirection.ltr,
-            textAlign: TextAlign.left,
-            style: ThemeTextStyles.rowValue,
-          )
-        ],
-      )
+      createRow("SoC vendor", deviceInfoMap["board"]),
+      new Padding(padding: EdgeInsets.only(top: 5.0)),
+      createRow("SoC model", deviceInfoMap["hardware"])
     ];
   }
 
   List<Widget> softwareWidgets() {
     return <Widget>[
-      new Text(
-          "Software",
-          style: ThemeTextStyles.homeCardTitle
-      ),
+      new Text("Software", style: ThemeTextStyles.homeCardTitle),
       new Container(
         color: Colors.white,
         width: 90.0,
         height: 1.0,
       ),
+      new Padding(padding: EdgeInsets.only(top: 10.0)),
+      createRow("Android Version",
+          deviceInfoMap["version"] + " (API " + deviceInfoMap["API"] + ")"),
       new Padding(padding: EdgeInsets.only(top: 5.0)),
-      new Row(
-        children: <Widget>[
-          Text(
-            "key",
-            textAlign: TextAlign.left,
-            style: ThemeTextStyles.rowKey,
-          ),
-          Padding(padding: EdgeInsets.only(right: 15.0)),
-          Text(
-            "value",
-            textDirection: TextDirection.ltr,
-            textAlign: TextAlign.left,
-            style: ThemeTextStyles.rowValue,
-          )
-        ],
-      ),
+      createRow("Android Security Patch", deviceInfoMap["secPatch"]),
       new Padding(padding: EdgeInsets.only(top: 5.0)),
-      new Row(
-        children: <Widget>[
-          Text(
-            "key",
-            textAlign: TextAlign.left,
-            style: ThemeTextStyles.rowKey,
-          ),
-          Padding(padding: EdgeInsets.only(right: 15.0)),
-          Text(
-            "value",
-            textDirection: TextDirection.ltr,
-            textAlign: TextAlign.left,
-            style: ThemeTextStyles.rowValue,
-          )
-        ],
-      )
+      createRow(
+          "Google Play Security Patch", deviceInfoMap["googlePlaySecPatch"])
     ];
+  }
+
+  Row createRow(String key, String value) {
+    return new Row(
+      children: <Widget>[
+        Text(
+          key,
+          textAlign: TextAlign.left,
+          style: ThemeTextStyles.rowKey,
+        ),
+        Padding(padding: EdgeInsets.only(right: 15.0)),
+        Text(
+          value,
+          textDirection: TextDirection.ltr,
+          textAlign: TextAlign.left,
+          style: ThemeTextStyles.rowValue,
+        )
+      ],
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'navigation.dart';
-import 'package:device_info/device_info.dart';
 import 'utils/theme.dart';
 
 void main() {
   runApp(SecGuard());
-  processorCheck();
 }
 
 class SecGuard extends StatelessWidget {
@@ -19,18 +17,4 @@ class SecGuard extends StatelessWidget {
       ),
     );
   }
-}
-
-Future<void> processorCheck() async {
-  DeviceInfoPlugin deviceInfo = DeviceInfoPlugin(); // instantiate device info plugin
-  AndroidDeviceInfo androidDeviceInfo = await deviceInfo.androidInfo; // instantiate Android Device Information
-  var board = androidDeviceInfo.board; //this will return you device model name
-  var manufacturer = androidDeviceInfo.manufacturer;
-  var model = androidDeviceInfo.model;
-  var hardware = androidDeviceInfo.hardware;
-
-  print("board: " + board);
-  print("manufacturer " + manufacturer);
-  print("model " + model);
-  print("hardware " + hardware);
 }
